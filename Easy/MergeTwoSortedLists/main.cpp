2a821aad9e2240a65f96e7577a8f259ec82d7ebf
Ruslan Pirakov <rpirakov20@gmail.com>
2024-11-01 18:13:58 +0700

    Add description of List Node for the task.

--- Easy/MergeTwoSortedLists/main.cpp
+++ Easy/MergeTwoSortedLists/main.cpp
@@ -1,23 +1,31 @@
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);
-        ListNode* current = &dummy;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+}; 
     
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val < list2->val) {
-                current->next = list1;
-                list1 = list1->next;
-            } else {
-                current->next = list2;
-                list2 = list2->next;
-            }
-            current = current->next;
-        }
-
-        if (list1 != nullptr) {
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode dummy(0);
+    ListNode* current = &dummy;
+  
+    while (list1 != nullptr && list2 != nullptr) {
+        if (list1->val < list2->val) {
             current->next = list1;
+            list1 = list1->next;
         } else {
             current->next = list2;
+            list2 = list2->next;
         }
-
-        return dummy.next;
+        current = current->next;
     }
+
+    if (list1 != nullptr) {
+        current->next = list1;
+    } else {
+        current->next = list2;
+    }
+
+    return dummy.next;
+}
